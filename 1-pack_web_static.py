b4744b2da254b42693be7b18a62440aeb995bfe8
jsgalvish@unal.edu.co <deimon@Aspire.localdomain>
2020-01-03 15:04:28 -0500

    refactor 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-# Generates a .tgz archive from the contents of the web_static folder
+"Generates a .tgz"
 
-from fabric.api import run, local, sudo
+import tarfile
+import os
 from datetime import datetime
 
 
 def do_pack():
-    """Packs web_static files into .tgz file"""
-
-    file_name = "versions/web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
-    local('mkdir -p versions')
-    command = local("tar -cvzf " + file_name + " ./web_static/")
-    if command.succeeded:
-        return file_name
-    return None
+    """ Create tar """
+    savedir = "versions/"
+    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+    with tarfile.open(savedir + filename, "w:gz") as tar:
+        tar.add("web_static", arcname=os.path.basename("web_static"))
+    if os.path.exists(savedir + filename):
+        return savedir + filename
+    else:
+        return None
